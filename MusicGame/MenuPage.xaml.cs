d49a5d97910c5dd02a8c1f5338fa764ba41ddf00
cb_27 <cwb@outlook.com>
2014-02-22 01:26:34 -0500

    Live Tile! Other UI fixes

--- MusicGame/MenuPage.xaml.cs
+++ MusicGame/MenuPage.xaml.cs
@@ -1,8 +1,14 @@
 ﻿using Microsoft.Phone.Controls;
+using Microsoft.Xna.Framework.Media;
 using System;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Net;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using Telerik.Windows.Controls;
 using Windows.System;
 
@@ -10,12 +16,62 @@ namespace MusicGame
 {
     public partial class MenuPage : PhoneApplicationPage
     {
+        AlbumCollection albums;
         public MenuPage()
         {
             InitializeComponent();
+            initalizeSongList();
+            randomizeLiveTile();
         }
 
-       
+        private void initalizeSongList()
+        {
+            MediaLibrary songs = new MediaLibrary();
+            albums = songs.Albums;
+        }
+
+        private void randomizeLiveTile()
+        {
+            RadFlipTileData tileData = new RadFlipTileData()
+            {
+                VisualElement = getAlbumArt(),
+                Title = "What's Spinning?",
+                BackVisualElement = getAlbumArt(),
+                MeasureMode = Telerik.Windows.Controls.MeasureMode.Element,
+                BackTitle = "What's Spinning?",
+                WideBackVisualElement = getAlbumArt(),
+                WideVisualElement = getAlbumArt(),
+                SmallVisualElement = getAlbumArt()
+            };
+            LiveTileHelper.CreateOrUpdateTile(tileData, new Uri("/", UriKind.Relative), true);
+        }
+
+        private Image getAlbumArt()
+        {
+            WriteableBitmap album = new WriteableBitmap(500,500);
+            album.SetSource(getRandomAlbumArt());
+            album.Invalidate();
+            Image finished = new Image() { Source = album };
+            return finished;
+        }
+
+        private Stream getLogo()
+        {
+            return App.GetResourceStream(new Uri("Assets/tileLarge.png", UriKind.Relative)).Stream;
+        }
+
+        private Stream getRandomAlbumArt()
+        {
+            Random rand = new Random();
+            Stream art = albums[rand.Next(albums.Count)].GetAlbumArt();
+            while (art == null)
+            {
+                art = albums[rand.Next(albums.Count)].GetAlbumArt();
+            }
+            return art;
+        }
+
+
         private void scores_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(new Uri("/HighScores.xaml", UriKind.Relative));
